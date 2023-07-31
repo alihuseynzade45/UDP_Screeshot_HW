f4415bf4d56504677e22019ce1f321c04e41929c
Standard Computer's <Standard Computer's@DESKTOP-FRC77TS>
2023-07-31 23:22:28 +0300

    Add project files.

---
+++ Server_/Program.cs
@@ -0,0 +1,73 @@
+﻿
+using System.Drawing;
+using System.Net;
+using System.Net.Sockets;
+using System.Drawing.Imaging;
+using System.Text;
+using System.Runtime.InteropServices;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var listener = new UdpClient(27001);
+        Console.WriteLine("The server is listening for connections...");
+
+        while (true)
+        {
+            IPEndPoint remoteEP = new IPEndPoint(IPAddress.Any, 0);
+            byte[] data = listener.Receive(ref remoteEP);
+            Console.WriteLine("The client is connected.");
+
+            byte[] screenshotData = GetScreenshot();
+
+
+            byte[] imageSizeBytes = Encoding.ASCII.GetBytes(screenshotData.Length.ToString());
+            listener.Send(imageSizeBytes, imageSizeBytes.Length, remoteEP);
+
+
+            int bufferSize = ushort.MaxValue - 29;
+            int totalBytesSent = 0;
+
+            while (totalBytesSent < screenshotData.Length)
+            {
+                int remainingBytes = screenshotData.Length - totalBytesSent;
+                int sendSize = Math.Min(bufferSize, remainingBytes);
+                byte[] buffer = new byte[sendSize];
+                Buffer.BlockCopy(screenshotData, totalBytesSent, buffer, 0, sendSize);
+                listener.Send(buffer, buffer.Length, remoteEP);
+                totalBytesSent += sendSize;
+            }
+            Console.WriteLine("Screenshot sent.");
+        }
+    }
+
+    private static byte[] GetScreenshot()
+    {
+        [DllImport("user32.dll")]
+        static extern int GetSystemMetrics(int nIndex);
+
+        const int SM_CXSCREEN = 0;
+        const int SM_CYSCREEN = 1;
+
+
+        int screenWidth = GetSystemMetrics(SM_CXSCREEN);
+        int screenHeight = GetSystemMetrics(SM_CYSCREEN);
+
+        Bitmap Image = new Bitmap(screenWidth, screenHeight);
+
+        Size s = new Size(Image.Width, Image.Height);
+
+
+        using (var bitmap = new Bitmap(screenWidth, screenHeight))
+        using (var graphics = Graphics.FromImage(bitmap))
+        {
+            graphics.CopyFromScreen(0, 0, 0, 0, bitmap.Size);
+            using (var memoryStream = new MemoryStream())
+            {
+                bitmap.Save(memoryStream, ImageFormat.Png);
+                return memoryStream.ToArray();
+            }
+        }
+    }
+}
---
+++ Socket_UDP_TCP_CLient_TCP_Listener_HW/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace Socket_UDP_TCP_CLient_TCP_Listener_HW { 
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            var client = new UdpClient();
+            try
+            {
+                var serverIp = IPAddress.Parse("127.0.0.1");
+                var serverPort = 27001;
+                var remoteEP = new IPEndPoint(serverIp, serverPort);
+
+                byte[] buffer = { 4 };
+                await client.SendAsync(buffer, buffer.Length, remoteEP);
+                buffer = new byte[ushort.MaxValue - 29];
+                var result = await client.ReceiveAsync();
+                int imageSize = int.Parse(Encoding.ASCII.GetString(result.Buffer));
+
+                var imageBuffer = new byte[imageSize];
+                int totalBytesReceived = 0;
+
+                while (totalBytesReceived < imageSize)
+                {
+                    result = await client.ReceiveAsync();
+                    var receivedSize = result.Buffer.Length;
+                    Buffer.BlockCopy(result.Buffer, 0, imageBuffer, totalBytesReceived, receivedSize);
+                    totalBytesReceived += receivedSize;
+                }
+                BuildImage(imageBuffer);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+            finally
+            {
+                client.Close();
+            }
+        }
+
+        public void BuildImage(byte[] imageBuffer)
+        {
+            using (MemoryStream ms = new MemoryStream(imageBuffer))
+            {
+                BitmapImage image = new BitmapImage();
+                image.BeginInit();
+                image.StreamSource = ms;
+                image.CacheOption = BitmapCacheOption.OnLoad;
+                image.EndInit();
+                screenShotImage.Source = image;
+            }
+        }
+    }
+}
